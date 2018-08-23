b22d725ad3ba7cf3ffbd9e7f3e68645a41ca50ef
Moritz Vogt <m.vogt@prosis.de>
2018-08-23 17:25:34 +0200

    Connect and Disconnect Handlers

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 
+// Configure Port for Heroku and local
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(publicPath))
+// Create http Server
+var server = http.createServer( app );
+// Create Web Socket Server
+var io = socketIO( server );
 
+io.on('connection', ( socket ) => {
+   console.log('new User connected')
 
-app.listen( port, () => {
-    console.log(`Server started on ${port}`);
+    socket.on('disconnect', () => {
+        console.log('User has disconnected')
+    });
 });
 
-module.exports = {
-    app
-}
+
+// Configure Middleware
+app.use(express.static(publicPath));
+
+server.listen( port, () => {
+    console.log(`Server started on ${port}`);
+});
